34ab6e8de1793715b829dea31754fd3c3d4da99d
mohammad alsaleh <malsaleh88@example.com>
2025-09-10 16:16:53 +0200

    Initial commit: Spring Boot project setup

---
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/UniversityEctsSystemApplication.java
@@ -0,0 +1,13 @@
+package be.ulb.ects.universityectssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityEctsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityEctsSystemApplication.class, args);
+	}
+
+}
---
+++ university-ects-system/src/test/java/be/ulb/ects/universityectssystem/UniversityEctsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ulb.ects.universityectssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityEctsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
