7c3dad1443ef084622a4efd4a6e6a72f6650cb3e
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 19:53:41 +0200

    feat: add getInscriptionsByMatricule method in InscriptionService

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/service/InscriptionService.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/service/InscriptionService.java
@@ -19,5 +19,8 @@ public class InscriptionService {
         return inscriptionRepository.findAll();
     }
 
+    public List<Inscription> getInscriptionsByMatricule(String matricule) {
+        return inscriptionRepository.findByMatricule(matricule);
+    }
 
 }
