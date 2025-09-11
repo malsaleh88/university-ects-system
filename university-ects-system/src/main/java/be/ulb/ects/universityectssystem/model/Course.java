0014779eae9c7da4d852e857b08a22871ecfdcd4
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 08:24:36 +0200

    Update dependencies, Course model, and DB config

---
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/model/Course.java
@@ -0,0 +1,20 @@
+package be.ulb.ects.universityectssystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "liste_cours")
+public class Course {
+    @Id
+    private String mnemonique;
+    private String intitule;
+    private int credit;
+    private String titulaire;
+}
