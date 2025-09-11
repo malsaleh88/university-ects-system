3993277f84969985ae45e6a6ed402a3a9bc5d38c
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 09:25:21 +0200

    feat: add StudentController with base path /api/students

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/StudentController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/StudentController.java
@@ -1,4 +1,11 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import be.ulb.ects.universityectssystem.model.Student;
+import be.ulb.ects.universityectssystem.repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
+
 }
