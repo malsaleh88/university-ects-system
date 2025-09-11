77b798ff7770b5a8b1fdcbeebb6ccb9b99074378
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 09:58:27 +0200

    feat: add NoteController with base path /api/

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/NoteController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/NoteController.java
@@ -1,4 +1,10 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import be.ulb.ects.universityectssystem.model.Note;
+import be.ulb.ects.universityectssystem.repository.NoteRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notes")
 public class NoteController {
 }
