44b1356f615e8df950428af8e9265d5585c11303
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 08:46:05 +0200

    feat: add CourseController with base path /api/courses

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/CourseController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/CourseController.java
@@ -1,4 +1,10 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+
 }
