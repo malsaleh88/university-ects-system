9059c18520cc65d68ac27e33182924c73fc68a4b
mohammad alsaleh <malsaleh88@example.com>
2025-09-11 08:46:57 +0200

    feat: inject CourseRepository using constructor

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/CourseController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/CourseController.java
@@ -1,10 +1,17 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import be.ulb.ects.universityectssystem.repository.CourseRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
 
 }
