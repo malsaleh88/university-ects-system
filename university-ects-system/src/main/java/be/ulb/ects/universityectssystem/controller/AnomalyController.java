8657a148cdb224f176533841bc34703958705fe2
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 13:43:54 +0200

    feat(anomalies): add anomaly reporting with DTO, service, and controller (part 2)

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/AnomalyController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/AnomalyController.java
@@ -1,4 +1,24 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import be.ulb.ects.universityectssystem.dto.AnomalyDto;
+import be.ulb.ects.universityectssystem.service.AnomalyService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/anomalies")
 public class AnomalyController {
+
+    private final AnomalyService anomalyService;
+
+    public AnomalyController(AnomalyService anomalyService) {
+        this.anomalyService = anomalyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnomalyDto>> getAllAnomalies() {
+        return ResponseEntity.ok(anomalyService.generateAnomalies());
+    }
 }
