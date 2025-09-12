187d309b902cc91181a9c9d89271648a8c57c3de
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 09:34:15 +0200

    chore(config): add RestTemplate bean configuration for external API calls

---
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package be.ulb.ects.universityectssystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
