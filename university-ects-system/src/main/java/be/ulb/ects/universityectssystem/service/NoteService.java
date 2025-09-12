528722ff1cadb528d2c8ed19d93d250fb08298ee
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 08:57:26 +0200

    feat: add NoteService to handle business logic for Note

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/service/NoteService.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/service/NoteService.java
@@ -1,4 +1,17 @@
 package be.ulb.ects.universityectssystem.service;
 
+import be.ulb.ects.universityectssystem.repository.NoteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    
+
 }
