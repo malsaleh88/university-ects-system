08f9d235196ce897f8031627759d79c7d6a3458e
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 11:59:49 +0200

    feat(part2): add BulletinController to expose bulletins API endpoint

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/BulletinController.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/controller/BulletinController.java
@@ -1,4 +1,26 @@
 package be.ulb.ects.universityectssystem.controller;
 
+import be.ulb.ects.universityectssystem.dto.BulletinDto;
+import be.ulb.ects.universityectssystem.service.BulletinService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bulletins")
 public class BulletinController {
+
+    private final BulletinService bulletinService;
+
+    public BulletinController(BulletinService bulletinService) {
+        this.bulletinService = bulletinService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BulletinDto>> getAllBulletins() {
+        return ResponseEntity.ok(bulletinService.generateBulletins());
+    }
 }
