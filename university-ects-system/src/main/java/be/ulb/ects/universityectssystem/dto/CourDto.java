1785caa2fca3a2257f54c3051877fbe87a82c57e
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 11:07:49 +0200

    fix(dto): add Lombok annotations to DTOs (InscriptionDto, CourDto, NoteDto, etc.) to enable JSON serialization in Part 2

--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/dto/CourDto.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/dto/CourDto.java
@@ -1,8 +1,12 @@
 package be.ulb.ects.universityectssystem.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourDto {
     private String mnemonique;
     private String intitule;
--- university-ects-system/src/main/java/be/ulb/ects/universityectssystem/dto/NoteDto.java
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/dto/NoteDto.java
@@ -1,8 +1,12 @@
 package be.ulb.ects.universityectssystem.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NoteDto {
     private int id;
     private String matricule;   // FK to student
