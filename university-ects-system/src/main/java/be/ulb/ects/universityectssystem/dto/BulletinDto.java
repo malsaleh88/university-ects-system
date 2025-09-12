c83ab6284086188843effc54fd4af96b0255b7d4
mohammad alsaleh <malsaleh88@example.com>
2025-09-12 10:58:59 +0200

    part2: add DTO for Bulltin to map external API responses

---
+++ university-ects-system/src/main/java/be/ulb/ects/universityectssystem/dto/BulletinDto.java
@@ -0,0 +1,20 @@
+package be.ulb.ects.universityectssystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BulletinDto {
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private int annee;
+    private int ectsTotalInscrits;
+    private int ectsObtenus;
+    private double moyennePonderee;
+    private boolean reussite;
+    private List<CourseDetailDto> details;
+}
