e0f0bfbbeb1dc449ddff7cec5bbdd28b7f168e1a
mohammad alsaleh <malsaleh88@example.com>
2025-09-13 19:58:26 +0200

    chore(frontend): create about page

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,15 @@
+
+import { Container } from "react-bootstrap";
+
+export default function About() {
+  return (
+    <Container style={{ marginTop: "30px" }}>
+      <h1>About</h1>
+      <p>
+        👋 Hello, my name is <strong>Mohammad Alsaleh</strong>.  
+        This project is part of the University ECTS System demo.
+      </p>
+    </Container>
+  );
+}
+
